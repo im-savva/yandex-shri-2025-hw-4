e3108b108bed611affed51f2047ad581f3dde124
Savva <60704556+im-savva@users.noreply.github.com>
2025-06-22 23:59:47 +0300

    v2

--- src/pages/History/History.tsx
+++ src/pages/History/History.tsx
@@ -1,8 +1,13 @@
+import { useState } from "react";
+import { createPortal } from "react-dom";
+import type { GalacticReport } from "../../App.types";
 import { Button } from "../../components/Button/Button";
 import { ButtonGroup } from "../../components/Button/ButtonGroup";
+import { HighlightsGrid } from "../../components/HighlightsGrid/HighlightsGrid";
 import { ReportRow } from "../../components/ReportsGroup/ReportRow";
 import { ReportsGroup } from "../../components/ReportsGroup/ReportsGroup";
 import { useStore } from "../../store/store";
+import CancelIcon from "../../components/icons/CancelIcon";
 
 export function HistoryPage() {
   const { reports, removeReport, clearReports } = useStore(
@@ -13,6 +18,10 @@ export function HistoryPage() {
     window.location.href = "/generate";
   };
 
+  const [selectedReport, setSelectedReport] = useState<GalacticReport | null>(
+    null
+  );
+
   return (
     <div className="layout-container">
       <div className="layout-group">
@@ -22,6 +31,7 @@ export function HistoryPage() {
               key={report.id}
               report={report}
               onDelete={({ id }) => removeReport(id)}
+              onClick={(report) => setSelectedReport(report)}
             />
           ))}
         </ReportsGroup>
@@ -42,6 +52,40 @@ export function HistoryPage() {
           )}
         </ButtonGroup>
       </div>
+      {selectedReport &&
+        createPortal(
+          <div
+            style={{
+              position: "fixed",
+              top: 0,
+              left: 0,
+              right: 0,
+              bottom: 0,
+              backgroundColor: "rgba(0, 0, 0, 0.5)",
+              zIndex: 1000,
+              overflow: "auto",
+            }}
+          >
+            <div
+              style={{
+                width: 800,
+                margin: "100px auto",
+                backgroundColor: "white",
+                padding: "20px",
+                borderRadius: "8px",
+              }}
+            >
+              <Button
+                theme="filled"
+                color="black"
+                icon={<CancelIcon />}
+                onClick={() => setSelectedReport(null)}
+              />
+              <HighlightsGrid isShrinked metrics={selectedReport} />
+            </div>
+          </div>,
+          document.body
+        )}
     </div>
   );
 }
