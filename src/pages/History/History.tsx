48a2bfbbc1b4349b68c07e40fcdb85bc74693b2c
Savva <60704556+im-savva@users.noreply.github.com>
2025-06-22 23:56:10 +0300

    v1

--- src/pages/History/History.tsx
+++ src/pages/History/History.tsx
@@ -1,18 +1,46 @@
+import { Button } from "../../components/Button/Button";
+import { ButtonGroup } from "../../components/Button/ButtonGroup";
 import { ReportRow } from "../../components/ReportsGroup/ReportRow";
 import { ReportsGroup } from "../../components/ReportsGroup/ReportsGroup";
 import { useStore } from "../../store/store";
 
 export function HistoryPage() {
-  const { reports } = useStore((state) => state.history);
+  const { reports, removeReport, clearReports } = useStore(
+    (state) => state.history
+  );
+
+  const redirectToGenerate = () => {
+    window.location.href = "/generate";
+  };
 
   return (
     <div className="layout-container">
       <div className="layout-group">
         <ReportsGroup>
           {reports.map((report) => (
-            <ReportRow key={report.id} report={report} />
+            <ReportRow
+              key={report.id}
+              report={report}
+              onDelete={({ id }) => removeReport(id)}
+            />
           ))}
         </ReportsGroup>
+        <ButtonGroup>
+          <Button
+            theme="filled"
+            color="green"
+            text="Сгенерировать больше"
+            onClick={redirectToGenerate}
+          />
+          {reports.length > 0 && (
+            <Button
+              theme="filled"
+              color="black"
+              text="Очистить все"
+              onClick={clearReports}
+            />
+          )}
+        </ButtonGroup>
       </div>
     </div>
   );
